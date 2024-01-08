d745911af39925bb6f189951f3a818c0c10b762c
adripower <adriantorres.gc@gmail.com>
2024-01-08 01:06:56 +0000

    modified:   exercises/008-getLengthOfTwoWords/app.js 	modified:   exercises/009-getLengthOfThreeWords/app.js 	modified:   exercises/010-isSameLength/app.js 	modified:   exercises/011-isGreaterThanTen/app.js 	modified:   exercises/012-isLessThanThirty/app.js

--- exercises/010-isSameLength/app.js
+++ exercises/010-isSameLength/app.js
@@ -1,1 +1,16 @@
-// Write your function here
+// Write your function 
+function isSameLength(val1,val2){
+if (val1.lenght === val2.lenght )return true;
+ else return false;
+
+}
+let output = isSameLength('words', 'super');
+console.log(output); //
+
+/* Se puede hacer mas corto el codigo 
+pero yo lo hice haci de complicado que soy.
+
+
+return word1.length === word2.length
+
+*/
--- exercises/012-isLessThanThirty/app.js
+++ exercises/012-isLessThanThirty/app.js
@@ -1,4 +1,7 @@
 function isLessThan30(num) {
   // your code here
-  
+  return (num <30);
+
 }
+let output = isLessThan30(8);
+console.log(output);
